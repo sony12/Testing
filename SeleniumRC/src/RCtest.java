c2a5a448feaf660dc146dd7a2dd5e8c833854eb3
sony12 <SelfService@SelfServices-MacBook-Pro.local>
2016-01-31 21:27:19 -0800

    Ticket2
    Check All

---
+++ SeleniumRC/src/RCtest.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
+
+
+public class RCtest {
+
+	public static void main(String[] args) {
+	 //Selenium 
+		//selenium=new DefaultSelenium("localhost",4444,"*firefox*","http://w3schools.com");
+		WebDriver driver=new FirefoxDriver();
+
+	}
+
+}
